37264dc815dc7eb0aec527088398737619236e6d
Trufla <mustafa.fci.cs@gmail.com>
2019-12-23 00:26:19 +0200

    init project

---
+++ CleanSwift/CleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanSwift
+//
+//  Created by Trufla on 12/23/19.
+//  Copyright © 2019 Trufla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
